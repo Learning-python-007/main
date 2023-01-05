20f01c1fb76ba877b1f1960e3f2e6eeae01c768e
Tatiana Gavrila <tatiana.gavrila@NB-SED6-4278.local>
2023-01-05 09:14:53 +0200

    teste2

--- Training-harvard/TEME CAP 1/deep-def.py
+++ Training-harvard/TEME CAP 1/deep-def.py
@@ -1,17 +1,27 @@
-def main():
-    x = input("What is the answer to the Great Question of Life, the Universe and Everything? ")
-    if answear(x):
-        print("Yes")
-    else:
-        print("No")
+# def main():
+#     x = input("What is the answer to the Great Question of Life, the Universe and Everything? ")
+#     if answear(x):
+#         print("Yes")
+#     else:
+#         print("No")
+#
+#
+# def answear(n):
+#     if n == ("42"):
+#         return True
+#     elif n == ("Forty-two"):
+#         return True
+#     elif n == ("forty two"):
+#         return True
+#
+# main()
 
+def is_answer_correct(answer):
+    answer = answer.lower().replace(' ', '')
+    return answer == '42' or answer == 'fortytwo'
 
-def answear(n):
-    if n == ("42"):
-        return True
-    elif n == ("Forty-two"):
-        return True
-    elif n == ("forty two"):
-        return True
-
-main()
+answer = input('What is the answer to the Great Question of Life, the Universe, and Everything? ')
+if is_answer_correct(answer):
+    print('Yes')
+else:
+    print('No')
